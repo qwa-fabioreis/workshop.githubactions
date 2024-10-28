9328bb16963f61aab6970b32c9505825bf3ab768
qwa-fabioreis <fabio.reis@qwasolucoes.com.br>
2024-10-28 08:50:26 -0300

    feat: adicionado logback para tarzer os logs

--- src/main/java/br/com/qwasolucoes/workshop/githubactions/controller/ClientController.java
+++ src/main/java/br/com/qwasolucoes/workshop/githubactions/controller/ClientController.java
@@ -2,6 +2,8 @@ package br.com.qwasolucoes.workshop.githubactions.controller;
 
 import br.com.qwasolucoes.workshop.githubactions.domain.Client;
 import br.com.qwasolucoes.workshop.githubactions.repository.ClientRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -21,6 +23,8 @@ import java.util.List;
 @RequestMapping("/clients")
 @CrossOrigin(maxAge = 3600)
 public class ClientController {
+
+    Logger logger = LoggerFactory.getLogger(ClientController.class);
     private final ClientRepository repository;
 
     public ClientController(ClientRepository repository){
@@ -28,15 +32,18 @@ public class ClientController {
     }
     @GetMapping
     public List<Client> getClients(){
+        logger.info("Listando todos clientes");
         return repository.findAll();
     }
     @GetMapping("/{id}")
     public Client getClient(@PathVariable Long id){
+        logger.info("Trazendo cliente com id: " + id);
         return repository.findById(id).orElseThrow(RuntimeException::new);
     }
     @PostMapping
     public ResponseEntity createClient(@RequestBody Client client) throws URISyntaxException{
         Client savedClient = repository.save(client);
+        logger.info("Salvndo novo cliente");
         return ResponseEntity.created(new URI("/clients/" + savedClient.getId())).body(savedClient);
     }
     @PutMapping("/{id}")
@@ -44,15 +51,16 @@ public class ClientController {
         Client currentClient = repository.findById(id).orElseThrow(RuntimeException::new);
         currentClient.setName(client.getName());
         currentClient.setEmail(client.getEmail());
-
         Client clientUpDated = repository.saveAndFlush(currentClient);
 
+        logger.info("Atualizando cliente com id: " + id);
         return ResponseEntity.ok(clientUpDated);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity deleteClient(@PathVariable Long id){
         repository.deleteById(id);
+        logger.info("Apagando cliente com id: " + id);
         return ResponseEntity.ok().build();
     }
 
